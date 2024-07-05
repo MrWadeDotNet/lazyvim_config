830f9084a6788685fb952f93f5287fcd8809db64
Lewis Wade <Lewis.Wade@transcard.com>
2024-07-05 16:00:33 -0500

    updated config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -64,21 +64,33 @@ local nvim_lsp = require("lspconfig")
 
 nvim_lsp.omnisharp.setup({
   cmd = {
-    "C:/omnisharp/OmniSharp.exe",
+    "C:/Users/lewis.wade/.omnisharp/OmniSharp.exe",
     "--languageserver",
     "--hostPID",
     tostring(vim.fn.getpid()),
     "--loglevel",
     "trace",
+    "DotNet:enablePackageRestore=true",
+    "--encoding",
+    "utf-8",
+    "FormattingOptions:EnableEditorConfigSupport=true",
+    "Sdk:IncludePrereleases=true",
   },
   filetypes = { "cs", "vb", "razor" },
   root_dir = nvim_lsp.util.root_pattern("*.csproj", "*.sln"),
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
   on_attach = function(client, bufnr)
+    print("LSP attached to buffer", bufnr) --Debugger
     vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
     vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(
+      "n",
+      "<F12>",
+      "<cmd>lua vim.lsp.buf.implementation()<CR>",
+      { noremap = true, silent = true }
+    )
     vim.api.nvim_set_keymap(
       "n",
       "<leader>q",
@@ -143,24 +155,71 @@ cmp.setup({
 
 require("trouble").setup({})
 
--- nvim-dap configuration
+local function get_process_list()
+  local handle = io.popen('tasklist /fi "IMAGENAME eq dotnet.exe" /fo csv /nh')
+  local result = handle:read("*a")
+  handle:close()
+  local process_list = {}
+  for id, name in string.gmatch(result, '"([^"]+)",[^,]+,"([^"]+)"') do
+    table.insert(process_list, { pid = id, name = name })
+  end
+  return process_list
+end
+
+local function pick_process(callback)
+  local process_list = get_process_list()
+  if #process_list == 0 then
+    print("No dotnet processes found")
+    return
+  end
+  local options = {}
+  for i, process in ipairs(process_list) do
+    table.insert(options, string.format("%s (%s)", process.name, process.pid))
+  end
+  vim.ui.select(options, { prompt = "Select process to attach:" }, function(choice)
+    for i, process in ipairs(process_list) do
+      if choice == string.format("%s (%s)", process.name, process.pid) then
+        callback(process.pid)
+        return
+      end
+    end
+  end)
+end
+
 local dap = require("dap")
 
 -- Adapter configuration for .NET
-dap.adapters.coreclr = {
-  type = "executable",
-  command = "path/to/vsdbg", -- Adjust this to the actual path of vsdbg
-  args = { "--interpreter=vscode" },
-}
-
--- Configuration for .NET
+--
 dap.configurations.cs = {
   {
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
-      return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+      return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/net6.0/", "file")
     end,
+    cwd = vim.fn.getcwd(),
+    stopAtEntry = false,
+    console = "integratedTerminal",
+  },
+  {
+    type = "coreclr",
+    name = "attach - netcoredbg",
+    request = "attach",
+    processId = pick_process,
+    cwd = vim.fn.getcwd(),
+    stopAtEntry = false,
   },
 }
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>db",
+  '<cmd>lua require"dap".toggle_breakpoint()<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<leader>dc", '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>di", '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>do", '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dr", '<cmd>lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>da", '<cmd>lua require"dap".attach()<CR>', { noremap = true, silent = true })
